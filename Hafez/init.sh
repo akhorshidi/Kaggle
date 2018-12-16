f83f4a65337b80c0fb9e5a2935a370788f3b2a3d
Mehran <akhorshidi@users.noreply.github.com>
2018-12-16 16:16:27 +0330

    Update init.sh

--- Hafez/init.sh
+++ Hafez/init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#test -d ./HafezAnalysis1 || mkdir HafezAnalysis1
-#test -f ./HafezAnalysis1/hafez.txt || wget -P ./HafezAnalysis1/ www.cs.cmu.edu/~nsharifr/hafez.txt
-#chmod +x ./hafez.sh ./init.sh
-
 if ! [ -x "$(command -v bicon)" ]; then 
 	echo "Missing Bicon! Try to install it."
 else 

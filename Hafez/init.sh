54ec3b879b6782ddd819187bd020a73ff4c83091
Mehran <akhorshidi@users.noreply.github.com>
2018-12-16 16:17:06 +0330

    Update init.sh

--- Hafez/init.sh
+++ Hafez/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Copyright (C) 2018 - A. KHORSHIDI <akhorshidi@moama.org>
+# This file is distributed in the hope that it will be useful;
+# It must be used under the terms of the GNU GPL v3.0.
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+
 if ! [ -x "$(command -v bicon)" ]; then 
 	echo "Missing Bicon! Try to install it."
 else 
